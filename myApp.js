631cb631efbafb0e8d175e2cb26e2174a4bdbbb3
FacundoTobioLilab <facundo.tobio@lilab.pe>
2024-04-16 17:12:05 -0300

    feat: se agrega la conexion a la BDD de mongoDBCloud

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+var mongoose = require('mongoose');
 
 let Person;
 
@@ -51,6 +51,8 @@ const queryChain = (done) => {
   done(null /*, data*/);
 };
 
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
  */
